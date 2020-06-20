99241fb1ced1403abe152094a649180b0ed05a89
vinayramu <vinay.ramu1@gmail.com>
2020-06-20 21:57:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var request = new XMLHttpRequest();
-request.open('GET','https://github.com/vinayramu/pspositions/blob/master/data.txt', false);
+request.open('GET','data.txt', false);
 request.send();
 console.log(request);
